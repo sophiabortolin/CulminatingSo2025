c72a484929f95befe8715adf0d059c6cf1fbc93c
sophia <sophia@localhost>
2025-12-05 13:12:48 -0500

    initial code drop

---
+++ CulminatingSophia/src/GUIDriver.java
@@ -0,0 +1,29 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class GUIDriver extends Application {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		// TODO Auto-generated method stub
+		VBox root = new VBox(10);
+		Label text = new Label("HELLO");
+		root.getChildren().add(text);
+		
+		Scene scene = new Scene(root, 600, 600);
+		stage.setScene(scene);
+		stage.show();
+		
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+
+	}
+
+}
