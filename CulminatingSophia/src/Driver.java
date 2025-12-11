924da1aa75d4326ebaf8eebde12901775708ebfc
sophia <sophia@localhost>
2025-12-11 15:20:31 -0500

    first code

--- CulminatingSophia/src/Driver.java
+++ CulminatingSophia/src/Driver.java
@@ -3,6 +3,23 @@ import java.util.ArrayList;
 public class Driver {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
-		}	
+		int width = 4;
+		int height = 4;
+		
+		int[][] grid = new int[width][height];
+		
+		display(grid);
+		
+	}	
+	public static void display(int[][] arr) {
+		int numRows = arr.length;
+		int numColumns = arr[0].length;
+		
+		for (int r = 0; r < numRows; r++) {
+			for (int c =0; c <numColumns; c++) {
+				System.out.print("0 ");
+			}
+			System.out.println();
+		}
+	}
 }
